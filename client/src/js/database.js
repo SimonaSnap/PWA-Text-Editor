e0ea7c8e47dde5fdbaebd5a6ac50b9738ff24c9f
Simona <simona.snapk@gmail.com>
2022-07-14 20:50:17 -0700

    added code to database and install

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -32,6 +32,21 @@ export const putDb = async (content) =>
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>
+{
+  const contactDb = await openDB("jate", 1);
+
+  const tx = contactDb.transaction("jate", "readonly");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log("result.value", result);
+
+  return result;
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = () =>
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [128],
+            sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
         ],
